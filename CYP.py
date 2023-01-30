f304c9c8cd978e8f77840e4a1a76170da2fe7ba8
VANECK <79650307+IvanIsak2000@users.noreply.github.com>
2023-01-30 20:43:25 +0300

    Create CYP.py

---
+++ CYP.py
@@ -0,0 +1,57 @@
+import os 
+import sys 
+
+try:
+
+    from genp import password_generation
+    import dearpygui.dearpygui as dpg
+
+except:
+
+    os.system('pip3 install genp')
+    from genp import password_generation
+
+    os.system('pip install dearpygui')
+    import dearpygui.dearpygui as dpg
+
+
+def create_password():
+    digit = dpg.get_value('digits')
+    letter = dpg.get_value('letters')
+    spec_symbol = dpg.get_value('special_symbols')
+    password_len = dpg.get_value('len')
+
+    try:
+        print(password_generation(digit,letter,spec_symbol,password_len))
+
+    except:
+        pass
+
+    
+
+dpg.create_context()
+dpg.create_viewport(title='Password Generation', width=400, height=450)
+with dpg.window(label=None, width=500, height=550, tag='Primary Window'):
+
+    digits = dpg.add_checkbox(label='Digits On?',tag = 'digits')
+
+    letters = dpg.add_checkbox(label='Letters On?',tag ='letters')
+    
+    special_symbols = dpg.add_checkbox(label='Special symbols On?',tag='special_symbols ')
+    
+    dpg.add_text('\nEnter password len:')
+    user_password_len = (dpg.add_input_int(tag='len'))
+    
+
+    dpg.add_button (label='Create password\n',callback = create_password)#create password 
+    
+
+ 
+dpg.setup_dearpygui()
+dpg.show_viewport()
+dpg.set_primary_window('Primary Window', True)
+dpg.start_dearpygui()
+
+
+
+
